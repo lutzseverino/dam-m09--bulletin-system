45a109717e9e666a62aa2cb116a38acdfec18be2
marcel <marcel.ggda@gmail.com>
2023-11-23 16:40:40 +0100

    He hecho la UI solo queda hacer lo de los semaforos y eso

--- Client/Program.cs
+++ Client/Program.cs
@@ -12,28 +12,35 @@
     {
         Random r = new Random();
 
-        Console.WriteLine($"Alumne: {nom} {cognom}");
-        Console.WriteLine($"Curs: DAM{curs}");
+        Console.WriteLine($"┌───────────────────────────┐");
+        Console.WriteLine($"│       Butlletí Escolar    │");
+        Console.WriteLine($"├───────────────────────────┤");
+        Console.WriteLine($"│ Alumne: {nom} {cognom}        │");
+        Console.WriteLine($"│ Curs: DAM{curs}                │");
 
         if (curs == 1)
         {
-            for(int i = 1; i < 6; i++)
+            for (int i = 1; i < 6; i++)
             {
                 var nota = r.Next(0, 11);
-                Console.WriteLine($"M0{i}: {nota}");
+                Console.WriteLine($"│ M0{i}: {nota,-3}                  │");
             }
-        } 
+        }
         else if (curs == 2)
         {
             for (int i = 6; i < 14; i++)
             {
                 var nota = r.Next(0, 11);
-                Console.WriteLine($"M0{i}: {nota}");
+                Console.WriteLine($"│ M0{i}: {nota,-3}                 │");
             }
         }
         else
         {
-            Console.WriteLine($"L'alumne {nom} {cognom} esta cursant el curs {curs} que es inexistent.");
+            Console.WriteLine($"│ L'alumne {nom} {cognom} esta cursant el curs {curs,-2} que es inexistent. │");
         }
+
+        Console.WriteLine($"└───────────────────────────┘");
+        Thread.Sleep(20000);
     }
+
 }
